c2ee52b82c72f76a5676fbd1d4617d1e5e35d84a
t1m0f3y <t.leonenko2001@gmail.com>
2021-07-20 16:30:02 +0700

    add some plot in txt_to_json.py file

--- txt_to_json.py
+++ txt_to_json.py
@@ -2,6 +2,10 @@ import copy
 import ctypes.wintypes
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def read_from_file(fileName):
     f = open(fileName,'r')
@@ -22,6 +26,13 @@ def read_from_file(fileName):
 
     return (lat,lon,storey)
 
+def check_cells(lon, lat, i, j, check_i, check_j):
+    if (check_i+i) < 0 or (check_i+i) >= lon or (check_j+j) < 0 or (check_j+j) >= lat:
+        return(0,0)
+
+    return(check_i,check_j)
+
+
 def main():
     lat,lon,storey = read_from_file('Novosibirsk_storeys.txt')
 
@@ -40,101 +51,78 @@ def main():
 
     print(len(storey))
 
-    mapA = copy.deepcopy(map)
-    mapB = copy.deepcopy(map)
+    #mapA = copy.deepcopy(map)
+
+    Z = np.zeros((latC,lonC))
 
     for i in range(lonC):
         for j in range(latC):
             map[i][j]=storey[i*latC+j]
+            Z[j][i]=storey[i*latC+j]
 
 
+    X = np.arange(0, lonC, 1)
+    Y = np.arange(0, latC, 1)
+    X, Y = np.meshgrid(X, Y)
 
+    fig = plt.figure()
+    ax = Axes3D(fig,auto_add_to_figure=False, box_aspect=(1*(lonC/latC),1,0.1))
+
+    fig.add_axes(ax)
+
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.viridis, antialiased=False, edgecolor ='none',linewidth=20)
+    ax.set(xlim=[0,lonC],ylim=[0,latC], zlim = [0,200])
+
+    plt.show()
+
+
+#test1
+"""
+    # setup the figure and axes
+    fig = plt.figure()
+    ax = Axes3D(fig,auto_add_to_figure=False)
+    fig.add_axes(ax)
+
+    # fake data
+    X = np.arange(0, lonC, 1)
+    Y = np.arange(0, latC, 1)
+    X, Y = np.meshgrid(X, Y)
+
+    width = depth = 1
+
+    ax.bar3d(X, Y, Z, width, depth, top, shade=True)
+    ax.set_title('Shaded')
+    plt.show()
+
+"""
+
+#test2
+"""
     buildingID=1
 
     stop=False
 
     while not stop:
         stop=True
-        print('a')
         for i in range(0,lonC):
             for j in range(0,latC):
-                if (i==0 and j==0) or (i==0 and j==latC-1) or (i==lonC-1 and j==0) or (i==lonC-1 and j==latC-1):
-                    if i == 0:
-                        if j == 0:
-                            if map[i][j] == map[i][j + 1]:
-                                if map[i][j] == 0:
-                                    mapA[i][j] = buildingID
-                            if map[i][j] == map[i + 1][j]:
-                                if map[i][j] == 0:
-                                    mapA[i][j] = buildingID
-                            if map[i][j] == map[i + 1][j + 1]:
-                                if map[i][j] == 0:
-                                    mapA[i][j] = buildingID
-                        else:
-                            if j == latC-1:
-                                if map[i][j] == map[i][j - 1]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                                if map[i][j] == map[i + 1][j - 1]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                                if map[i][j] == map[i + 1][j]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                    else:
-                        if i == lonC-1:
-                            if j == 0:
-                                if map[i][j] == map[i - 1][j]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                                if map[i][j] == map[i - 1][j + 1]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                                if map[i][j] == map[i][j + 1]:
-                                    if map[i][j] == 0:
-                                        mapA[i][j] = buildingID
-                            else:
-                                if j == latC - 1:
-                                    if map[i][j] == map[i - 1][j - 1]:
-                                        if map[i][j] == 0:
-                                            mapA[i][j] = buildingID
-                                    if map[i][j] == map[i - 1][j]:
-                                        if map[i][j] == 0:
-                                            mapA[i][j] = buildingID
-                                    if map[i][j] == map[i][j - 1]:
-                                        if map[i][j] == 0:
-                                            mapA[i][j] = buildingID
-                else:
-                    if map[i][j] == map[i - 1][j - 1]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i - 1][j]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i - 1][j + 1]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i][j - 1]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i][j + 1]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i + 1][j - 1]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i + 1][j]:
-                        if map[i][j]==0:
-                            mapA[i][j]=buildingID
-                    if map[i][j] == map[i + 1][j + 1]:
-                        if map[i][j]==0:
+                for check_i in range(-1,2):
+                    for check_j in range(-1,2):
+                        check_i , check_j = check_cells(lonC,latC,i,j,check_i,check_j)
+                        if map[i][j]==map[i+check_i][j+check_j] and mapA[i][j]==0 and map[i][j] is not map[i+check_i][j+check_j]:
                             mapA[i][j]=buildingID
 
-        for i in range(0,lonC):
-            for j in range(0,latC):
-                if mapA[i][j]==0:
-                    stop=True
-        buildingID+=1
+                        if mapA[i][j] == 0:
+                            stop = False
+                            buildingID += 1
+
+        for i in range(lonC):
+            print(mapA[i])
+        print("--------------------------------------------")
+
+                    #print("i = " + str(i))
+                    #print("j = " + str(j))
+                    #print("buildingID = " + str(buildingID))
 
     for i in range(lonC):
         print(mapA[i])
@@ -142,6 +130,8 @@ def main():
     print("-----")
     for i in range(lonC):
         print(map[i])
+"""
+
 
 if __name__ == '__main__':
     main()
