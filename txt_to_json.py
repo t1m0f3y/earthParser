7255c4b585df07a096c15f251afaf8a01bea4e12
t1m0f3y <t.leonenko2001@gmail.com>
2021-07-22 12:45:55 +0700

    add json file

--- txt_to_json.py
+++ txt_to_json.py
@@ -26,6 +26,32 @@ def read_from_file(fileName):
 
     return (lat,lon,storey)
 
+
+def to_json(lat,latC,lon,lonC,storey,Z):
+
+
+    data={'ID':None,'borders':{'x':latC,'y':lonC,'minlon':lon[0], 'minlat':lat[0], 'maxlon':lon[len(lon)-1], 'maxlat':lat[len(lat)-1]}}
+
+    data.update({'ID':{str(int(Z[0][0])):{'lon':[lon[0]], 'lat':[lat[0]], 'storeys':[storey[0]], 'x':[0], 'y':[0]}}})
+
+    for i in range(lonC):
+        for j in range(latC):
+            if str(int(Z[i][j])) not in data['ID'].keys():
+                data['ID'][str(int(Z[i][j]))] ={'lon':[lon[i]], 'lat':[lat[j*lonC + i]], 'storeys':storey[i*latC+j], 'x':[j], 'y':[i]}
+            else:
+                data['ID'][str(int(Z[i][j]))]['lon'].append(lon[i])
+                data['ID'][str(int(Z[i][j]))]['lat'].append(lat[j*lonC + i])
+                data['ID'][str(int(Z[i][j]))]['x'].append(j)
+                data['ID'][str(int(Z[i][j]))]['y'].append(i)
+
+
+    with open('storeys.json','w') as file:
+        json.dump(data,file, indent=1)
+
+
+
+
+
 def check_cells(lon, lat, i, j, check_i, check_j):
     if (check_i+i) < 0:
         check_i=0
@@ -45,22 +71,16 @@ def check_cells(lon, lat, i, j, check_i, check_j):
 def plot():
     lat,lon,storey = read_from_file('Novosibirsk_storeys.txt')
 
-    x=lat[0]
-
     latC = 0
     while lat[0] == lat[latC]:
         latC += 1
 
-    j=0
-    i=0
 
     lonC = int(len(lon)/latC)
 
     map = [[0 for j in range(latC)] for i in range(lonC)]
 
     print(len(storey))
-
-    #mapA = copy.deepcopy(map)
 
     Z = np.zeros((lonC,latC))
 
@@ -111,8 +131,6 @@ def check(origin_i,origin_j, i,j,map,mapA, lonC, latC, bulID):
 def get_buildings_boundaries():
     lat, lon, storey = read_from_file('Novosibirsk_storeys.txt')
 
-    x = lat[0]
-
     latC = 0
     while lat[0] == lat[latC]:
         latC += 1
@@ -123,13 +141,11 @@ def get_buildings_boundaries():
     lonC = int(len(lon) / latC)
 
     map = [[0 for j in range(latC)] for i in range(lonC)]
-    #mapA = [[0 for j in range(latC)] for i in range(lonC)]
 
     Z = np.zeros((lonC,latC))
 
     print(len(storey))
 
-    #mapA = copy.deepcopy(map)
 
     for i in range(lonC):
         for j in range(latC):
@@ -137,9 +153,6 @@ def get_buildings_boundaries():
 
 
     buildingID=2
-
-    stop=False
-
 
 
     for i in range(0,lonC,1):
@@ -154,22 +167,6 @@ def get_buildings_boundaries():
                 buildingID+=1
 
 
-
-
-     ##   for i in range(lonC):
-      ##      print(Z[i])
-       ## print("--------------------------------------------")
-
-
-
-###
-
-
-##    for i in range(lonC):
-##        for j in range(latC):
- ##           if Z[i][j]!=333:
-  ##              Z[i][j]=1
-
     X = np.arange(0, latC, 1)
     Y = np.arange(0, lonC, 1)
     X, Y = np.meshgrid(X, Y)
@@ -178,7 +175,6 @@ def get_buildings_boundaries():
     ax = Axes3D(fig,auto_add_to_figure=False, box_aspect=(1,1*(lonC/latC),0.1))
 
     fig.add_axes(ax)
-
 
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap="hot", antialiased=False, edgecolor ='none',linewidth=20)
     ax.set(xlim=[0,latC],ylim=[0,lonC], zlim = [0,100000000000000])
@@ -189,69 +185,15 @@ def get_buildings_boundaries():
 
     plt.show()
 
+    to_json(lat,latC,lon,lonC,storey,Z)
 
+# N: 210, 211, 217, 254, 268
 
 
 def main():
 
     #plot()
     get_buildings_boundaries()
-
-#test1
-"""
-    # setup the figure and axes
-    fig = plt.figure()
-    ax = Axes3D(fig,auto_add_to_figure=False)
-    fig.add_axes(ax)
-
-    # fake data
-    X = np.arange(0, lonC, 1)
-    Y = np.arange(0, latC, 1)
-    X, Y = np.meshgrid(X, Y)
-
-    width = depth = 1
-
-    ax.bar3d(X, Y, Z, width, depth, top, shade=True)
-    ax.set_title('Shaded')
-    plt.show()
-
-"""
-
-#test2
-"""
-    buildingID=1
-
-    stop=False
-
-    while not stop:
-        stop=True
-        for i in range(0,lonC):
-            for j in range(0,latC):
-                for check_i in range(-1,2):
-                    for check_j in range(-1,2):
-                        check_i , check_j = check_cells(lonC,latC,i,j,check_i,check_j)
-                        if map[i][j]==map[i+check_i][j+check_j] and mapA[i][j]==0 and map[i][j] is not map[i+check_i][j+check_j]:
-                            mapA[i][j]=buildingID
-
-                        if mapA[i][j] == 0:
-                            stop = False
-                            buildingID += 1
-
-        for i in range(lonC):
-            print(mapA[i])
-        print("--------------------------------------------")
-
-                    #print("i = " + str(i))
-                    #print("j = " + str(j))
-                    #print("buildingID = " + str(buildingID))
-
-    for i in range(lonC):
-        print(mapA[i])
-
-    print("-----")
-    for i in range(lonC):
-        print(map[i])
-"""
 
 
 if __name__ == '__main__':
