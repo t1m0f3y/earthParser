75366abd8a80f845e3f03161acc363da9f74bf6c
t1m0f3y <t.leonenko2001@gmail.com>
2021-07-25 22:30:25 +0700

    add some features

--- Parser.py
+++ Parser.py
@@ -1,15 +1,49 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 class Parser:
-    'Here is Parser!'
+    """Here is Parser!"""
+    #__slots__ = []
     def __init__(self):
         pass
 
-    def smth(self):
-        print("A")
+    def readFromTxtFile(self,fileName, *args):
+        """Read .txt file's columns and convert they to one list of lists"""
+        f = open(fileName, 'r')
+        if f:
+            data = f.readlines()
 
+            lists=[[]for i in args]
+            for n in args:
+                for line in range(0, len(data)):
+                    lists[args.index(n)].append(data[line].split()[n])
+        return (lists)
+
+    def __check_cells(self, lon, lat, i, j, checkI, checkJ):
+        if (checkI + i) < 0:
+            checkI = 0
+        else:
+            if (checkI + i) >= lon:
+                checkI = 0
+
+        if (checkJ + j) < 0:
+            checkJ = 0
+        else:
+            if (checkJ + j) >= lat:
+                checkJ = 0
+
+        return (checkI, checkJ)
+
+    def plot(self, filename, x, y, z, x_name, y_name, z_name, z_lim):
+        pass
+
+    def __del__(self):
+        pass
 
 def main():
-    par = Parser()
-    par.smth()
+    pars = Parser()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
